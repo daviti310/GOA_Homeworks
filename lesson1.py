1c318eb6001bf5fe0b6a93e3389bb04b552a43dc
daviti310 <davitabuladze55@gmail.com>
2024-09-14 11:32:40 +0400

    first commit

---
+++ lesson1.py
@@ -0,0 +1,33 @@
+# name = "daviti"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "daviti" არის ცვლადისთვის მნიშვნელობა
+
+surname = "abuladze"
+
+# print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "daviti" #ეს არის str (string) ტიპის ცვლადი
+age = 13 # ეს არის int (integer) მთელი რიცხვი
+height = 155.5 #ეს არის float ტიპის ცვლადი
+#boolean (bool) ტიპის ცვლადი 
+
+knows_programming = True #True ან False
+is_ugly = False   #snakecase( უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+
+#print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# print(name + age)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+print(name + " " + str(age))
