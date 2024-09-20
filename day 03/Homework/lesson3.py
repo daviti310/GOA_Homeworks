494f9c636cea18fa7fcca00c7e93e543aeeef0e5
daviti310 <davitabuladze55@gmail.com>
2024-09-20 16:52:50 +0400

    commit

---
+++ day 03/Homework/lesson3.py
@@ -0,0 +1,69 @@
+'''შექმენით მინიმუმ 5 ცვლადი, რომლებშიც შეინახავთ თქვენი ოჯახის წევრების სახელებს (მაგალითად: mother: "Tamo"), შემდეგ კი დაბეჭდავთ ერთ დიდ წინადადებაში (მაგალითად print("my mother is" + " " + mother))'''
+#ჩემი ოჯახის წევრების სახელებს ვბეჭდავ
+mymotheris = "my mother is"
+mother = "sofio chitava"
+print(mymotheris + " " + mother)
+
+
+
+myfatheris = "my father is"
+father = "irakli abuladze"
+print(myfatheris + " " + father)
+
+
+
+mybrotheris = "my brother is"
+brother = "iakob abuladze"
+print(mybrotheris + " " + brother)
+
+
+
+mysisteris = "my sister is"
+sister = "mariam abuladze"
+print(mysisteris + " " + sister)
+
+
+
+mygrandmotheris = "my grandmother is"
+grandmother = "dodo kaxidze"
+print(mygrandmotheris + " " + grandmother)
+
+
+
+'''თქვენი სასურველ რიცხვებს შორის შეასრულეთ და დაბეჭდეთ ოპერაციები, მინიმუმ 5 თითოეულ ოპერატორზე (+,-,*,//)'''
+#შევკრიბოთ, გამოვაკლოთ, გავამრავლოთ და გავყოთ რიცხვები
+print(27 + 32)
+print(23 - 19)
+print(46 * 54)
+print(63 // 9)
+
+
+
+print(35 + 27)
+print(12 - 5)
+print(87 * 95)
+print(91 // 13)
+
+
+
+print(80 + 135)
+print(49 - 37)
+print(30 * 25)
+print(75 // 5)
+
+
+
+print(90 + 23)
+print(88 - 43)
+print(86 * 54)
+print(76 // 2)
+
+
+
+print(106 + 324)
+print(111 - 15)
+print(395 * 453)
+print(255 // 5)
+
+
+
