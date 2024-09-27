4aa02af0568d08da0c09285fed32937e15c10583
daviti310 <davitabuladze55@gmail.com>
2024-09-27 18:53:44 +0400

    commit

---
+++ day 04/Classwork/test7.py
@@ -0,0 +1,12 @@
+'''შექმენით 2 ცვლადი რომელშიც შეინახავთ რიცხვით მნიშვნელობებს, შემდეგ კი მათ შორის შემდეგი ოპერაციების შედეგებს დაბეჭდავთ (+,-,*,/)'''
+
+
+number1 = 34
+
+number2 = 32
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
---
+++ day 04/Homework/lesson4.py
@@ -0,0 +1,52 @@
+''' შეასწორეთ (debug) შემდეგი კოდი:'''
+
+# გავასწოროთ შეცდომა
+
+name = "Alice"
+print(name)
+
+salary = 75000
+age = 30
+role = "Software Engineer"
+print(salary) 
+print(role)
+
+
+
+
+'''შექმენით 3 ცვლადი რომლებშიც შეინახავთ რიცხვით integer მნიშვნელობებს, შემდეგ სამივეს შორის დაბეჭდეთ შემდეგი ოპერაციების (+,-,*,/) შედეგები'''
+
+# შევკრიბოთ, გამოვაკლოთ, გავამრავლოთ და გავყოთ რიცხვები
+
+num1 = 125
+num2 = 100
+num3 = 12
+
+print(num1 + num2 + num3)
+print(num1 - num2 - num3)
+print(num1 * num2 * num3)
+print(num1 / num2 / num3)
+
+
+
+num4 = 345
+num5 = 234
+num6 = 110
+
+
+print(num4 + num5 + num6)
+print(num4 - num5 - num6)
+print(num4 * num5 * num6)
+print(num4 / num5 / num6)
+
+
+
+num7 = 1234
+num8 = 543
+num9 = 439
+
+
+print(num7 + num8 + num9)
+print(num7 - num8 - num9)
+print(num7 * num8 * num9)
+print(num7 / num8 / num9)
