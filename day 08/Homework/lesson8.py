05ba9e816588051d59bb7f23cd59c923353780f8
daviti310 <davitabuladze55@gmail.com>
2024-10-25 19:59:56 +0400

    commit

---
+++ day 08/Homework/lesson8.py
@@ -0,0 +1,49 @@
+# String ტიპის ცვლადები
+str1 = "vaja"
+str2 = "pshavela"
+str3 = "akaki"
+str4 = "wereteli"
+str5 = "ilia"
+str6 = "wavwavadze"
+str7 = "davit"
+str8 = "kakabadze"
+str9 = "elene"
+str10 = "axvlediani"
+
+# კონკატინაციის მაგალითი
+concat1 = str1 + " " + str2
+concat2 = str3 + " " + str4
+concat3 = str5 + " " + str6
+concat4 = str7 + " " + str8
+concat5 = str9 + " " + str10
+
+print(concat1)
+print(concat2)
+print(concat3)
+print(concat4)
+print(concat5)
+
+# Istrnteger ტიპის ცვლადები
+int1 = 5
+int2 = 10
+int3 = 15
+int4 = 20
+int5 = 25
+int6 = 30
+int7 = 35
+int8 = 40
+int9 = 45
+int10 = 50
+
+# მათემატიკური ჯამის მაგალითი
+sum1 = int1 + int2
+sum2 = int3 + int4
+sum3 = int5 + int6
+sum4 = int7 + int8
+sum5 = int9 + int10
+
+print(sum1)
+print(sum2)
+print(sum3)
+print(sum4)
+print(sum5)
