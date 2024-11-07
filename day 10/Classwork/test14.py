81414f3c643766bcc5bf7422ab716b503187a543
daviti310 <davitabuladze55@gmail.com>
2024-11-07 21:02:15 +0400

    commit

---
+++ day 10/Classwork/test14.py
@@ -0,0 +1,13 @@
+''' დაბეჭდეთ 3 სხვადასხვა ტიპის მნიშვნელობის მონაცემთა ტიპი'''
+
+print(type(7))
+print(type("GOA"))
+print(type(5.5))
+
+
+
+name = input("enter your name: ")
+age = input("enter your age: ")
+
+print(type(name))
+print(type(age))
