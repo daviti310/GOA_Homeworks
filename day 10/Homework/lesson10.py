004c5778a5039870f54a76e111826db687d235b6
daviti310 <davitabuladze55@gmail.com>
2024-11-09 13:19:50 +0400

    commit

---
+++ day 10/Homework/lesson10.py
@@ -0,0 +1,12 @@
+''' შექმენით ცვლადი, რომელშიც შეინხავთ input ინსტრუქციით შემოტანილ მნიშვნელობას, შემდეგ შეამოწმებთ თუ რა ტიპის მონაცემი ინახება ამ ცვლადში და დაპრინტავთ.'''
+
+
+
+singer = input("Enter your favorite singer: ")
+actor = input("Enter your favorite actor: ")
+athlete = input("Enter your favorite athlete: ")
+
+print(type(singer))
+print(type(actor))
+print(type(athlete))
+
