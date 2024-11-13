0ac4a06bc07cdb12a26dee71dd037e9cbb08dea5
daviti310 <davitabuladze55@gmail.com>
2024-11-13 20:25:07 +0400

    commit

---
+++ day 11/Classwork/test.py
@@ -0,0 +1,16 @@
+'''მომხმარებელს შემოატანინენთ სახელი, გვარი, ასაკი, ემაილი, პაროლი და თქვენთვის საინტერესო კიდევ 2 მონაცემი, შემდეგ შეინახეთ ცვლადში და საბოლოოდ დაბეჭდეთ ინფორმაცია ერთ დიდ წინადადებაში'''
+
+name = input("Enter your name: ")
+last_name = input("Enter your last_name: ")
+age = input("Enter your age: ")
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+
+print(name)
+print(last_name)
+print(age)
+print(email)
+print(password)
+
+print(name + last_name + age + email + password)
+
