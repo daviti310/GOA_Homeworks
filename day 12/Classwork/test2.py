056f32a20fc381d4efaee1595e2738d26c7049d1
daviti310 <davitabuladze55@gmail.com>
2024-11-15 15:53:43 +0400

    commit

---
+++ day 12/Classwork/test2.py
@@ -0,0 +1,7 @@
+''' მომხმარებელს შემოატანინეთ ასაკი დაუმატეთ 5 და შემდეგ დაუბეჭდეთ წინადადება ასეთ ფორმატში: 'You will be <ასაკი> years old after 5 years' ' '''
+
+age = int(input("enter your age: "))
+age1 = age + 5
+print("You will be" + " " + str(age1) + " " + "years old after 5 years" )
+
+
---
+++ day 12/Homework/lesson16.py
@@ -0,0 +1,12 @@
+'''მარტივი გამომთვლელი: შექმენით პროგრამა რომელიც მომხმარებელს შემოატაინინებს 2 რიცხვს და შეინახავს ცვლადებში, გარდაქმენით ეს რიცხვები integer-ად, შემდეგ კი დაბეჭდეთ მათ შორის 4 მათემატიკური ოპერაცია (+,-,*,//)'''
+
+year = input("Enter your favorite year: ")
+number = input("Enter your favorite number: ")
+
+print(int(year) + int(number))
+print(int(year) - int(number))
+print(int(year) * int(number))
+print(int(year) // int(number))
+
+
+
