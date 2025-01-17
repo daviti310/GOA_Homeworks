24c5d1f6a6f858c836d04659ef5ef2fa3ed5f41b
daviti310 <davitabuladze55@gmail.com>
2025-01-17 14:58:43 +0400

    commit

---
+++ day 22/Classwork/test1.py
@@ -0,0 +1,19 @@
+'''გამოიტანეთ თქვენი სახელი და გვარი ტერმინალში 10-ჯერ, პირველ რიგში გამოიყენეთ virgin გზა, შემდეგ კი chad-ური გზა. თქვენი სიტყვებით ახსენით რატომ ჯობია მეორე გზა, რა უპირატესობები აქვს'''
+# virgin გზა
+print("Davit Abuladze")
+print("Davit Abuladze")
+print("Davit Abuladze")
+print("Davit Abuladze")
+print("Davit Abuladze")
+print("Davit Abuladze")
+print("Davit Abuladze")
+print("Davit Abuladze")
+print("Davit Abuladze")
+print("Davit Abuladze")
+
+# chad გზა
+for i in range(10):
+    print("Davit Abuladze")
+
+# chad გზა ჯობია virgin გზას იმიტომ რომ არ გვაკარგვინებს დროს კოდის წერაში.
+
