2ab66aa14814c8dc0fcea0873eae7fb8889a143e
daviti310 <davitabuladze55@gmail.com>
2025-01-31 20:15:59 +0400

    commit

---
+++ day 24/Homework/lesson26.py
@@ -0,0 +1,38 @@
+
+i = 0
+while i <= 19:
+    print(i)
+    i += 1
+
+i = 10
+while i <= 30:
+    print(i)
+    i += 1
+
+i = 15
+while i <= 80:
+    print(i)
+    i += 1
+
+i = 50
+while i >= 0:
+    print(i)
+    i -= 1
+
+i = 0
+while i <= 50:
+    print(i)
+    i += 3
+
+i = 0
+while i <= 50:
+    print(i)
+    i += 2
+
+i = 100
+while i >= 20:
+    print(i)
+    i -= 3
+
+
+
